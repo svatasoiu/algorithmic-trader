0d5d007efbb5bad7fc6a8958f6e7132025e702be
svatasoiu <sorin_vatasoiu@brown.edu>
2014-07-22 10:23:14 -0400

    made printer parameter optional and added coloring in terminal

--- src/testSuite.ml
+++ src/testSuite.ml
@@ -16,28 +16,33 @@ let empty_ref = fun _ -> ref None
 let add_test_case_to_suite tc = test_suite := tc::!test_suite
 
 let print_results = 
-	fun _ -> Printf.printf "%d tests passed/%d tests failed/%d exceptions\n" !numPassed !numFailed !numExcept
+	fun _ -> Printf.printf "\027[32m%d tests passed/%d tests failed/%d exceptions\n" !numPassed !numFailed !numExcept
 
 let handle_test_result test_id printer act exp t = 
 	try print_string ((if t 
-										 then (incr numPassed; "[SUCC]: (") 
-										 else (incr numFailed; "[FAIL]: " ^ (printer (get_option !act)) ^ " not comp to " ^ (printer exp) ^ " (")) ^ test_id ^ ")\n")
-	with _ -> incr numExcept
+										 then (incr numPassed; "\027[32m[SUCC]: (") 
+										 else (incr numFailed; "\027[31m[FAIL]: " ^ 
+														match printer with
+																None -> ""
+															| Some printer -> (printer (get_option !act)) ^ " not comp to " 
+																							^ (printer exp) ^ " (")) 
+														^ test_id ^ ")\n")
+	with _ -> (incr numExcept; print_string ("\027[31m[EXCP]: " ^ test_id ^ "\n"))
 
 (* appends an async test case to test_suite
 	 that checks the result of (comp res (f arg)) 
 	 with the proper test_id *)
-let assert_test comp act exp test_id printer = 
+let assert_test comp act exp ?printer test_id = 
 	let p = empty_ref () in
 	try add_test_case_to_suite (act 
 														>>| (fun t -> p := Some t; comp t exp)
 														>>| handle_test_result test_id printer p exp)
-	with EmptyRef -> (incr numExcept; print_string ("[EXCP]: " ^ test_id ^ "\n"))
+	with EmptyRef -> (incr numExcept; print_string ("\027[31m[EXCP]: " ^ test_id ^ "\n"))
 
 (* appends an async test case to test_suite
 	 that checks the equality of res and (f arg) 
 	 with the proper test_id *)
-let assert_eq act exp test_id printer = assert_test (=) act exp test_id printer
+let assert_equal act exp ?printer test_id = assert_test (=) act exp ?printer:printer test_id
 
 (* runs all tests in test_suite and then shutsdown the scheduler *)
 let run_suite = 
--- src/test_analyzer.ml
+++ src/test_analyzer.ml
@@ -5,39 +5,39 @@ module A = Analyzer.EmptyAnalyzer(Scraper.BasicScraper)
 module E = Analyzer.EqualAnalyzer(Scraper.BasicScraper)
 
 let _ = 
-	assert_eq 
+	assert_equal 
 		(Scraper.BasicScraper.get_data ["Open";"PreviousClose";"ChangeinPercent"] ["GOOG"] >>| A.analyze)
 		A.empty_portfolio
 		"Empty Analyzer"
-		A.portfolio_to_string;;
+		~printer:A.portfolio_to_string;;
 
 let _ = 
-	assert_eq 
+	assert_equal 
 		(Scraper.BasicScraper.get_data ["Open";"PreviousClose";"ChangeinPercent"] ["GOOG"] >>| A.analyze)
 		A.empty_portfolio
 		"Empty Analyzer 2"
-		A.portfolio_to_string;;
+		~printer:A.portfolio_to_string;;
 
 let _ = 
-	assert_eq 
+	assert_equal 
 		(Scraper.BasicScraper.get_data ["Open";"PreviousClose";"ChangeinPercent"] ["GOOG"] >>| A.analyze)
 		(A.add_to_portfolio A.empty_portfolio "A" 0.5)
 		"Empty Analyzer fail"
-		A.portfolio_to_string;;
+		~printer:A.portfolio_to_string;;
 
 let _ =
-	assert_eq 
+	assert_equal 
 		(Scraper.BasicScraper.get_data ["Open";"PreviousClose";"ChangeinPercent"] ["GOOG"] >>| E.analyze)
 		(E.add_to_portfolio E.empty_portfolio "GOOG" 1.0)
 		"Equal Analyzer 1 stock"
-		E.portfolio_to_string;;
+		~printer:E.portfolio_to_string;;
 
 let _ =
-	assert_eq 
+	assert_equal 
 		(Scraper.BasicScraper.get_data ["Open";"PreviousClose";"ChangeinPercent"] ["GOOG";"MSFT"] >>| E.analyze)
 		[("GOOG",0.5);("MSFT",0.5)]
 		"Equal Analyzer 2 stocks"
-		E.portfolio_to_string;;
+		~printer:E.portfolio_to_string;;
 
 run_suite ();;
 
